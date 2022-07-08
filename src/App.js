cb354b256b478a390556d53996ae10135648b388
ZadAndRot <kasirecivanna@gmail.com>
2022-07-08 14:21:17 +0300

    state in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
-import Container from '../src/container';
+import Container from './components/container';
+
+const stateInfo={
+  good: 0,
+    neutral: 0,
+    bad: 0,
+    total:0,
+    positivePercentage:"0%"
+}
 
 function App() {
+
+  const state={
+    ...stateInfo,
+    // good: 0,
+    // neutral: 0,
+    // bad: 0,
+    // total:0,
+    // positivePercentage:"0%"
+  };
+
+  
   return (
     <div className="App">
-      <Container/>
+      <Container 
+      state={state}
+      
+      />
       
     </div>
   );
--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -1,26 +1,41 @@
 import React from "react";
-import Statistics from "../components/statistics";
-import FeedbackOptions from "../components/feedback";
-import Section from "../components/section";
-import Notification from "../components/message";
+import Statistics from "../statistics";
+import FeedbackOptions from "../feedback";
+import Section from "../section";
+import Notification from "../message";
 
 
 class Container extends React.Component{
-
-    state={
-        good: 0,
-        neutral: 0,
-        bad: 0,
-        total:0,
-        positivePercentage:"0%"
-    };
     
+    constructor({state}){
+        super();
+        this.state=state;
+        
+
+    }
+    onLeaveFeedback=(e)=>{
+        if(e.target.value==="good"){
+            this.goodPoint();
+        }else if(e.target.value==="neutral"){
+            this.neutralPoint();
+        }else if(e.target.value==="bad"){
+            this.badPoint();
+        }
+          
+    };
 
     totalPoint=()=>{
-        this.setState(prevState=>({positivePercentage:prevState.good/(prevState.good+prevState.neutral+prevState.bad)*100+"%"}));
-        this.setState(prevState=>({total:prevState.good+prevState.neutral+prevState.bad}))
+        this.countTotalFeedback();
+        this.countPositiveFeedbackPercentage();
     };
-
+    countTotalFeedback=()=>{
+        this.setState(prevState=>({total:prevState.good+prevState.neutral+prevState.bad}))
+    }
+    countPositiveFeedbackPercentage=()=>{
+        this.setState(prevState=>({positivePercentage:prevState.good/(prevState.good+prevState.neutral+prevState.bad)*100+"%"}));
+        
+    }
+  
     goodPoint=()=>{
         this.setState(prevState=>({good:prevState.good+1}));
         this.totalPoint();
@@ -34,26 +49,13 @@ class Container extends React.Component{
         this.totalPoint();
     };
 
-    onLeaveFeedback=(e)=>{
-        if(e.target.value==="good"){
-            this.goodPoint();
-        }else if(e.target.value==="neutral"){
-            this.neutralPoint();
-        }else if(e.target.value==="bad"){
-            this.badPoint();
-        }
-            
-    }
-
-    
-
     render(){
         const {good,neutral,bad,total,positivePercentage} =this.state;
         // добавити options
         
         return(
             <Section title="Please leave feedback">
-                <FeedbackOptions options={"d"} onLeaveFeedback={this.onLeaveFeedback}/>
+                <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback}/>
                 <h2>Statistics</h2>
                 {this.state.good===0 && this.state.bad===0 && this.state.neutral===0?<Notification message="There is no feedback"/>:<Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/>}
             </Section>
