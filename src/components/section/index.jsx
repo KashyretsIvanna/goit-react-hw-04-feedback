3dc63f992772ab19b3993308f72be2e2b093f1ec
ZadAndRot <kasirecivanna@gmail.com>
2022-07-24 21:56:39 +0300

    feat: :bug:

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -11,7 +11,7 @@ const Section = props => {
 
 Section.propTypes = {
 	title: PropTypes.string.isRequired,
-	children: PropTypes.array.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 export default Section;
